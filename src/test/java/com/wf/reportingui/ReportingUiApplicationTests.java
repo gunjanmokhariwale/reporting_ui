85f68ce37ed555ce75a3847dea40f41fe5e8bb10
gunjanmokhariwale <gunjan_mokhariwale@persistent.com>
2023-10-12 18:17:02 +0530

    Initial commit

---
+++ src/test/java/com/wf/reportingui/ReportingUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wf.reportingui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
